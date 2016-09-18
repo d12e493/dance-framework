d6e729ab03f94e41765316ea71ac0bda18d94602
Davis Chen <Davis Chen@192.168.0.109>
2016-09-18 21:26:11 +0800

    加入entity column

--- dance-entity/src/main/java/idv/danceframework/entity/UserInfo.java
+++ dance-entity/src/main/java/idv/danceframework/entity/UserInfo.java
@@ -1,10 +1,44 @@
 package idv.danceframework.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by Davis on 2016/9/18.
  */
-//@Entity
-//@Schema
-//@Table
+@Entity
+@Table(schema = "SYSTEM", name = "USER_INFO")
 public class UserInfo {
+	@Id
+	@GeneratedValue
+	private Long usid;
+
+	private String name;
+	private String password;
+
+	public Long getUsid() {
+		return usid;
+	}
+
+	public void setUsid(Long usid) {
+		this.usid = usid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
