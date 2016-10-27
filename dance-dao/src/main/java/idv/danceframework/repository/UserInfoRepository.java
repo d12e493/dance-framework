a1ebd4e6f7afeec7716d78fb0a1a272722469153
Davis Chen <Davis@chenjialinde-MacBook-Air.local>
2016-10-27 12:30:23 +0800

    sna 調整資料使用

--- dance-dao/src/main/java/idv/danceframework/repository/UserInfoRepository.java
+++ dance-dao/src/main/java/idv/danceframework/repository/UserInfoRepository.java
@@ -1,8 +1,36 @@
 package idv.danceframework.repository;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 import idv.danceframework.entity.UserInfo;
 
 public interface UserInfoRepository extends BaseRepository<UserInfo, Long> {
-	
+
 	UserInfo findByEmail(String email);
+
+	@Select("select pid from sna.pkg_list where pid <=10 order by pid asc")
+	List<Integer> findPidList();
+
+	@Select("select imei from sna.imei_list where status = 0 limit 1000")
+	List<String> findAllImei();
+
+	@Select("select distinct pid from sna.imei_app where imei = #{imei} order by pid asc")
+	List<Integer> findPidListByImei(String imei);
+
+	@Select("select weight from sna.app_weight where pid1 = #{0} and pid2 = #{1}")
+	Integer findAppWeight(int p1, int p2);
+
+	@Insert("insert into sna.app_weight values(#{0},#{1},1)")
+	void insertAppWeight(int p1, int p2);
+
+	@Update("update sna.app_weight set weight = #{count} where pid1 = #{0} and pid2 = #{1}")
+	void updateAppWeight(int p1, int p2, @Param("count") int count);
+
+	@Update("update sna.imei_list set status = 1 where imei = #{imei}")
+	void updateImei(String imei);
 }
--- dance-dao/src/test/java/idv/danceframework/repository/UserInfoRepositoyTest.java
+++ dance-dao/src/test/java/idv/danceframework/repository/UserInfoRepositoyTest.java
@@ -1,9 +1,11 @@
 package idv.danceframework.repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
 
 import com.github.pagehelper.PageHelper;
 
@@ -13,7 +15,35 @@ import idv.danceframework.lo.UserInfoLO;
 import idv.danceframework.qo.PageRequest;
 import idv.danceframework.qo.UserInfoQO;
 
+@Rollback(value = false)
 public class UserInfoRepositoyTest extends BaseRepositoryTest<UserInfoRepository> {
+
+	private class Mapping {
+		private int primary;
+		private int remain;
+
+		public Mapping(int x, int y) {
+			this.primary = x;
+			this.remain = y;
+		}
+
+		public int getPrimary() {
+			return primary;
+		}
+
+		public void setPrimary(int primary) {
+			this.primary = primary;
+		}
+
+		public int getRemain() {
+			return remain;
+		}
+
+		public void setRemain(int remain) {
+			this.remain = remain;
+		}
+
+	}
 
 	@Test
 	@Ignore
@@ -23,6 +53,7 @@ public class UserInfoRepositoyTest extends BaseRepositoryTest<UserInfoRepository
 	}
 
 	@Test
+	@Ignore
 	public void findAll() {
 		UserInfoQO qo = new UserInfoQO();
 
@@ -32,4 +63,45 @@ public class UserInfoRepositoyTest extends BaseRepositoryTest<UserInfoRepository
 
 		System.out.println(page);
 	}
+
+	@Test
+	public void forSna() {
+		List<String> imeiList = repository.findAllImei();
+		for (String imei : imeiList) {
+			List<Integer> pidList = repository.findPidListByImei(imei);
+			List<Mapping> mappingList = transfer(pidList);
+
+			for (Mapping mapping : mappingList) {
+
+				int p1 = mapping.getPrimary();
+				int p2 = mapping.getRemain();
+
+				// find mapping row
+				Integer weight = repository.findAppWeight(p1, p2);
+				// if not exist insert
+				if (weight == null) {
+					repository.insertAppWeight(p1, p2);
+				}
+				// or update
+				else {
+					repository.updateAppWeight(p1, p2, weight + 1);
+				}
+			}
+			repository.updateImei(imei);
+		}
+	}
+
+	private List<Mapping> transfer(List<Integer> list) {
+		List<Mapping> mappingList = new ArrayList<Mapping>();
+		for (Integer primary : list) {
+
+			List<Integer> remainList = list.subList(list.indexOf(primary) + 1, list.size());
+
+			for (Integer remain : remainList) {
+				mappingList.add(new Mapping(primary, remain));
+			}
+		}
+		return mappingList;
+	}
+
 }
